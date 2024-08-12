b9d35867ef23751eec1aa69b7ee3b4496a8bd177
Adalyn Black <tomblackaz@gmail.com>
2024-08-12 11:39:32 -0700

    Update to 1.21.1

--- forge/src/main/java/com/adibtw/pistonvolume/forge/PistonVolumeForge.java
+++ forge/src/main/java/com/adibtw/pistonvolume/forge/PistonVolumeForge.java
@@ -1,15 +1,20 @@
 package com.adibtw.pistonvolume.forge;
 
 import com.adibtw.pistonvolume.PistonVolume;
-import dev.architectury.platform.forge.EventBuses;
+import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 
 @Mod(PistonVolume.MOD_ID)
 public class PistonVolumeForge {
     public PistonVolumeForge() {
-        // Submit our event bus to let architectury register our content on the right time
-        EventBuses.registerModEventBus(PistonVolume.MOD_ID, FMLJavaModLoadingContext.get().getModEventBus());
+        IEventBus modEventBus = FMLJavaModLoadingContext.get().getModEventBus();
+        modEventBus.addListener(this::commonSetup);
+    }
+
+    private void commonSetup(final FMLCommonSetupEvent event)
+    {
         PistonVolume.init();
     }
 }
